0c886313955fd1f3a666e5e3fcb84d9ad10303bd
Edu Alcover <alcover.edu@gmail.com>
2018-07-17 16:03:56 +0200

    jquery & script

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,31 @@
-alert()
+
+var funcion = function(e){
+    $('.ext').empty();
+    //console.log(e.target.value)
+    
+    //console.log(e.target.value.length)
+    
+    let a=[];
+    for (let i = e.target.value.length-1; i > 0; i--) {
+        a.push(e.target.value[i])
+        
+    }
+    //console.log(a)
+    let index = a.indexOf('.');
+    //console.log(index)
+    let b=[];
+    for (let x = index-1; x > -1; x--) {
+        b.push(a[x])
+        
+    }
+    console.log(b)
+    b=b.toString();
+    console.log(typeof(b))
+    console.log(b)
+    if(!(b.toUpperCase()==='CSV' || b.toLowerCase()==='c,s,v')){
+    //let file = file1.substr(index);
+    $('.ext').append(b);
+    }
+}
+
+$("input[name='file']").change(funcion);
